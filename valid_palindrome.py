d41a683cd9bf616c6501e5dfdb1e4899b576c97c
rahulgopireddy <rgopired@uncc.edu>
2024-01-08 16:33:48 +0530

    valid palindrome leetcode

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,21 +1,24 @@
 
-str = 'A'
+str = 'amanaplanacanalpanama'
 def is_palindrome(s):
-  # Write your code here
+    # 125. Valid Palindrome
+  # Write your code here  
   # Tip: You may use the code template provided
-  left = 0 
-  right = len(s) - 1
-  i = 1
-  if left == right:
-    return True
-  else: 
-    while left < right : 
-      if s[left] != s[right]:
-          return False
-      left = left + 1
-      right = right - 1 
-      i = i + 1
-  return True
+   result_string= re.sub('[^a-zA-Z0-9]','',s).lower()
+        print(result_string)
+        left = 0
+        right = len(result_string) - 1
+        i = 1
+        if left == right:
+            return True
+        else: 
+            while(left < right):
+                if (result_string[left] != result_string[right]):
+                    return False
+                left = left + 1
+                right = right -1 
+                i = i + 1
+        return True
 
 # is_palindrome(str)
 
